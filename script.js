c8b205838cd19d6f43350b72a40e4e6cd2beafa8
Piyush Yadav <85944872+piy-crpto@users.noreply.github.com>
2022-01-28 09:33:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,40 @@ menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
-window.onscroll = ()=>
-{
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
-}
-document.querySelector('#search-icon').onclick=()=>{
-  document.querySelector('#search-form').classList.toggle('activesearchform');
-}
-document.querySelector('#close').onclick=()=>{
-  document.querySelector('#search-form').classList.remove('activesearchform');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
 }
 
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
 
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
@@ -55,6 +77,7 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
 function loader(){
   document.querySelector('.loader-container').classList.add('fade-out');
 }
@@ -64,5 +87,3 @@ function fadeOut(){
 }
 
 window.onload = fadeOut;
-
-
